e104a18ec7a923c5f7cd1689bc0fba4726761512
Sofia <sofiakulymanova@gmail.com>
2020-05-16 00:23:19 +0400

    docker

--- vkrS/vkrS/Controllers/HomeController.cs
+++ vkrS/vkrS/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,8 @@ namespace vkrS.Controllers
             ViewBag.Title = "Home Page";
             SelectList images = new SelectList(db.Images, "ImageId", "Link");
             ViewBag.Images = images;
+            SelectList ts = new SelectList(db.TimeSeries, "TimeSeriesId", "Elements");
+            ViewBag.TimeSeries = ts;
 
             return View();
         }
@@ -25,6 +29,45 @@ namespace vkrS.Controllers
         {
             //проверка на наличие в бд
             return Redirect("~/TimeSeries/GetTimeSeriesAdmin");
+        }
+
+        [HttpPost]
+        public void AddImage(string imageInput)
+        {
+            if (imageInput != null)
+            {
+                db.Images.Add(new Image { ImageId = Guid.NewGuid(), Link = imageInput });
+                db.SaveChanges();
+            }
+        }
+
+        [HttpPost]
+        public void Start(string imageInput, string ts)
+        {
+            if (imageInput != null && ts!=null)
+            {
+                Guid imId = Guid.Parse(imageInput);
+                Guid tsId = Guid.Parse(ts);
+                var image = db.Images.FirstOrDefault(i => i.ImageId == imId);
+                var timeseries = db.TimeSeries.FirstOrDefault(t => t.TimeSeriesId == tsId);
+                if (image != null && timeseries != null)
+                {
+                    var timeStart = DateTime.Now;
+                    Process process = new Process { StartInfo = new ProcessStartInfo { FileName = "cmd.exe", RedirectStandardInput = true, RedirectStandardOutput = true, UseShellExecute = false, Arguments= "/c docker pull " + image.Link } };
+
+                    process.Start();
+                    
+                    process.WaitForExit();
+                    // не v8, а имя !!!
+                    process.StartInfo=new ProcessStartInfo {  FileName = "cmd.exe", RedirectStandardInput = true, RedirectStandardOutput = true, UseShellExecute = false, Arguments = "/c docker run -e ARRAY=" + timeseries.Elements.Replace(Environment.NewLine, "") + " v8" } ;
+                    process.Start();
+                    StreamReader srIncoming = process.StandardOutput;
+
+                    string result = srIncoming.ReadToEnd();
+                    var time = DateTime.Now - timeStart;
+
+                }
+            }
         }
     }
 }
